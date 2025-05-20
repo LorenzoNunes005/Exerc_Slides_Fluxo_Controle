70a03c3952a64738bebbaab4e580dc0100bba73d
LorenzoNunes005 <lorenzonunesiff@gmail.com>
2025-05-19 21:34:49 -0300

    Add files via upload

---
+++ Ex 2 - Switch Case.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+    double n1, n2, resultado;
+    char operador;
+
+    printf("Digite o primeiro número: ");
+    scanf("%lf", &n1);
+    printf("Digite o segundo número: ");
+    scanf("%lf", &n2);
+    printf("Selecione o operador: (+, -, *, /):");
+    scanf(" %c", &operador);
+    switch (operador){
+        case '+':
+            resultado = n1 + n2;
+            printf("Resultado: %.2lf\n", resultado);
+            break;
+        case '-':
+            resultado = n1 - n2;
+            printf("Resultado: %.2lf\n", resultado);
+            break;
+        case '*':
+            resultado = n1 * n2;
+            printf("Resultado: %.2lf\n", resultado); 
+            break; 
+        case '/':
+            resultado = n1 / n2;
+            printf("Resultado: %.2lf\n", resultado);
+            break;
+        default :
+            printf("Opção inválida\n");
+            break;  
+    }
+
+}
