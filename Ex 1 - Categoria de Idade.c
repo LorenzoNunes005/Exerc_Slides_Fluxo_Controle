43110392f24ff5861c4fd53f8c7af9054f7b5224
LorenzoNunes005 <lorenzonunesiff@gmail.com>
2025-05-19 21:07:16 -0300

    Add files via upload

---
+++ Ex 1 - Categoria de Idade.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int idade;
+    printf("Informe sua idade: ");
+    scanf("%d", &idade);
+    if (idade>=0 && idade<=12){
+        printf("Você é uma criança.");
+    }else{
+        if (idade>=13 && idade<=17){
+            printf("Você é um adolescente.");
+        }else{
+            if(idade >=18 && idade<=59){
+                printf("Você é um adulto.");
+            }else{
+                if(idade>=60){
+                    printf("Você é um idoso.");
+                }
+            }
+        }
+    }
+
+    return 0;
+}
